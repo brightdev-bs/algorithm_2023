aeca1d5e2a0b9c00eeaa4622308505df197e2b3a
Byeongsu KIM <brightdev.bs@gmail.com>
2023-11-06 22:49:42 +0900

    알고리즘 공부

--- src/core/dynamic/Q11066R.java
+++ src/core/dynamic/Q11066R.java
@@ -37,21 +37,49 @@ public class Q11066R {
 
     static FastReader sc = new FastReader();
 
+    static int[][] sum;
 
     public static void main(String[] args) {
         int TC = sc.nextInt();
         while(TC-- > 0) {
             int K = sc.nextInt();
             int[] files = new int[K + 1];
+            sum = new int[K + 1][K + 1];
             for (int i = 1; i <= K; i++) {
                 files[i] = sc.nextInt();
             }
 
+            pro(files, K);
 
+            int[][] solve = solve(files, K);
+            System.out.println(solve[1][K]);
         }
     }
 
-    private static void solve(int[] files, int K) {
+    private static void pro(int[] files, int K) {
+        for (int i = 1; i <= K; i++) {
+            for (int j = i; j <= K; j++) {
+                sum[i][j] = sum[i][j - 1] + files[j];
+            }
+        }
+    }
 
+    private static int[][] solve(int[] files, int K) {
+        int[][] dp = new int[K + 1][K + 1];
+        for (int len = 2; len <= K; len++) {
+            for (int i = 1; i <= K - len + 1; i++) {
+                int j = i + len - 1;
+
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = Math.min(
+                            dp[i][j],
+                            dp[i][k] + dp[k + 1][j] + sum[i][j]
+                    );
+                }
+            }
+        }
+
+        return dp;
     }
 }
